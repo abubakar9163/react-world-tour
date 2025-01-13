dd4c67c28f8d5ede74e2fd5a5e711d5dfc328a84
Abubakar9163 <rahman15-5851@diu.edu.bd>
2025-01-13 19:38:38 +0600

    add added all folder

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-  const {name,flags}=country;
+const Country = ({country,handleVisitedCountry, handleVisitedFlags}) => {
+  const {name,flags,population,area,cca3}=country;
+  const[visited,setVisited]=useState(false);
+  
+  
+  const handleVisited=()=>{
+    setVisited(!visited);
+  }
+  console.log( handleVisitedFlags);
+
     return (
-        <div className='country'>
-            <h3>Name:{name?.common}</h3>
+        <div className={`country ${visited?'visited':'non-visited'}`}>
+            <h3 style={{color:visited?'purple':'white'}}>Name:{name?.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population:{population}</p>
+            <p>area:{area}</p>
+            <p><small>Code:{cca3}</small></p>
+        
+
+            <button onClick={()=>handleVisitedCountry(country)}>Mark Visited</button>
+
+            <br />
+            <button onClick={()=>handleVisitedFlags(country.flags.png)}>Add flag</button>
+
+            <br />
+            <button onClick={handleVisited}>{visited?"visited":'goging'}</button>
+            { visited && 'I have visited'}
         </div>
     );
 };
